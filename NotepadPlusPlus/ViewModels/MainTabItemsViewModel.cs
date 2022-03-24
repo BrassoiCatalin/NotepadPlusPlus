3b53ab84f9a49154fa9da8c8c1642d13d5f3a423
BrassoiCatalin <brassoicatalin14@gmail.com>
2022-03-24 10:12:00 +0200

    Beautified the .xaml panels a bit

--- NotepadPlusPlus/ViewModels/MainTabItemsViewModel.cs
+++ NotepadPlusPlus/ViewModels/MainTabItemsViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Net.Mime;
 using System.Text.RegularExpressions;
 using System.Windows;
